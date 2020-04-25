c65868d9912ff327daa4811d94d54eaefd39b15c
Oleksii Skopiuk <aztek2008@gmail.com>
2020-04-26 00:29:11 +0300

    Added JS-HW-8

--- javascript/goit-js-hw-08/js/task-1.js
+++ javascript/goit-js-hw-08/js/task-1.js
@@ -1,45 +1,33 @@
 'use strict';
 
-//                     Разметка элемента галереи
-// Ссылка на оригинальное изображение должна храниться в data-атрибуте source на элементе img,
-// и указываться в href ссылки (это необходимо для доступности).
-
 import gallery from './gallery-items.js';
-// Создание и рендер разметки по массиву данных и предоставленному шаблону.
-// ->
-// создать объект, который содержит в себе масив элементов галереи с набором свойств:
-// - открытие модального окна по клику на элементе галереи.
-// - для этого добавлять на div.lightbox CSS-класс is-open
-// - закрытие модального окна по клику на кнопку button[data-action="close-modal"].
-// - очистка значения атрибута src элемента img.lightbox__image.
-// создать функцию, которая вставляет шаблон элемента галлереи
+
 const galleryGrid = document.querySelector('ul.js-gallery');
 const modalWindow = document.querySelector('div.js-lightbox');
 const modalWindowCloseBtn = document.querySelector('button.lightbox__button');
-const galeryItems = gallery.map(galeryItem => buildGalleryElement(galeryItem)).join('');
-const galeryItemDescription = gallery.map(galeryItem => galeryItem.description);
-console.dir(gallery)
-console.dir(galeryItemDescription)
+const galeryItems = gallery
+  .map(galeryItem => buildGalleryElement(galeryItem))
+  .join('');
+const galeryItemOriginals = gallery.map(galeryItem => galeryItem.original);
+const currentImage = document.querySelector('.lightbox__image');
+// console.log(galeryItemOriginals)
 
+{
+  // const galleryElements = {
+  //   elements: [],
+  //   add(picture) {
+  //     const element = picture;
+  //     this.elements.push(element);
 
-const galleryElements = {
-  elements: [],
-  add(picture) {
-    const element = picture;
-    this.elements.push(element);
+  //     return element;
+  //   },
+  // };
+} // -- НУЖЕН ЛИ ЭТОТ ОБЪЕКТ?
 
-    return element;
-  }
-};
+galleryGrid.addEventListener('mouseover', openModal);
+// galleryGrid.addEventListener('click', openModal);  --  НЕ РАБОТАЕТ?
 
-// const galeryItem = galleryElements.add()
-
-// console.dir(galeryItems)
-
-// galleryGrid.addEventListener('click', openModal);
-
-appendGalleryElement(galleryGrid, galeryItems)
-
+appendGalleryElement(galleryGrid, galeryItems);
 
 function buildGalleryElement(e) {
   return `
@@ -48,47 +36,38 @@ function buildGalleryElement(e) {
     href="${e.original}">
     <img
       class="gallery__image"
-      src="${e.preview}"
-      data-source="${e.original}"
+        src="${e.preview}"
+        data-source="${e.original}"
       alt="${e.description}"
     />
   </a>
 </li>
-
- `
-};
-
-buildGalleryElement()
-
-function appendGalleryElement(parentRef, galleryEl) {
-  parentRef.insertAdjacentHTML('beforeend', galleryEl)
+ `;
 }
 
-// function openModal(e) {
-//   // const galleryContainer = e.currentTarget;
-//   // modalWindow.classList.add('is-open');
+function appendGalleryElement(parentRef, galleryEl) {
+  parentRef.insertAdjacentHTML('beforeend', galleryEl);
+}
 
-//   if (e.target !== e.currentTarget) {
-//     modalWindow.classList.add('is-open');
-//   }
+function openModal(e) {
+  modalWindow.addEventListener('click', closeModal);
+  document.addEventListener('keydown', closeModal);
+  // document.addEventListener('keydown', event => {
+  //   console.dir(event.code);
+  // });
 
-//   const activeLink = e.currentTarget.querySelector('.is-open');
+  if (e.target === e.currentTarget) {
+    return;
+  }
+  modalWindow.classList.add('is-open');
+  currentImage.srcset = e.target.dataset.source;
+  currentImage.alt = e.target.alt;
+}
 
-//   if (activeLink) {
-//     activeLink.classList.remove('is-open');
-//   }
-
-//   e.target.classList.add('nav__link--active')
-// };
-
-// Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
-//
-// Открытие модального окна по клику на элементе галереи.
-//
-// Подмена значения атрибута src элемента img.lightbox__image.
-//
-// Закрытие модального окна по клику на кнопку button[data-action="close-modal"].
-//
-// Очистка значения атрибута src элемента img.lightbox__image.
-// Это необходимо для того, чтобы при следующем открытии модального окна,
-// пока грузится изображение, мы не видели предыдущее.
+function closeModal(e) {
+  if (e.target === modalWindowCloseBtn || e.target === 'Escape') {
+    modalWindow.classList.remove('is-open');
+    modalWindow.removeEventListener('click', closeModal);
+    currentImage.srcset = '';
+  }
+};
